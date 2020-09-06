383aecee5dabe0cd2a292f5e49c89b0a483a71b2
Tomasz Olszak <olszak.tomek@gmail.com>
2020-09-06 12:06:15 +0200

    bla bla 5

--- final_project-master/src/main/java/pl/sda/final_project/FinalProjectApplication.java
+++ final_project-master/src/main/java/pl/sda/final_project/FinalProjectApplication.java
@@ -3,6 +3,9 @@ package pl.sda.final_project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class FinalProjectApplication {
 
--- final_project-master/src/main/java/pl/sda/final_project/model/user/UserEntity.java
+++ final_project-master/src/main/java/pl/sda/final_project/model/user/UserEntity.java
@@ -12,46 +12,46 @@ import java.util.List;
 
 
 
-    @Entity
+@Entity
 
-    public class UserEntity extends BaseEntity {
+public class UserEntity extends BaseEntity {
 
-        private String firstName;
-        private String lastName;
-        @Embedded
-        private Address address;
-        private LocalDate birthDate;
-        private String pesel;
-        private String login;
-        private String password;
-        private String phoneNumber;
-        private boolean preferEmails;
-        @ManyToMany
-        private List<UserRole> roles;
+    private String firstName;
+    private String lastName;
+    @Embedded
+    private Address address;
+    private LocalDate birthDate;
+    private String pesel;
+    private String login;
+    private String password;
+    private String phoneNumber;
+    private boolean preferEmails;
+    @ManyToMany
+    private List<UserRole> roles;
 
-        public static UserEntity apply(RegistrationDto registrationDto, String pswdHash) {
-            UserEntity user = new UserEntity();
-            user.firstName = registrationDto.getFirstName();
-            user.lastName = registrationDto.getLastName();
-            user.login = registrationDto.getLogin();
-            user.password = pswdHash;
-            user.address = Address.apply(registrationDto);
-            user.birthDate = LocalDate.parse(registrationDto.getBirthDate(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-            return user;
-        }
-
-        public void addRole(UserRole userRole) {
-            if (roleExists(userRole)) {
-                return;
-            }
-            if (roles == null) {
-                roles = new ArrayList<>();
-            }
-            roles.add(userRole);
-        }
-
-        private boolean roleExists(UserRole userRole) {
-            return roles != null && roles.stream().anyMatch(r -> userRole.getRoleName().equals(r.getRoleName()));
-        }
+    public static UserEntity apply(RegistrationDto registrationDto, String pswdHash) {
+        UserEntity user = new UserEntity();
+        user.firstName = registrationDto.getFirstName();
+        user.lastName = registrationDto.getLastName();
+        user.login = registrationDto.getLogin();
+        user.password = pswdHash;
+        user.address = Address.apply(registrationDto);
+        user.birthDate = LocalDate.parse(registrationDto.getBirthDate(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        return user;
     }
+
+    public void addRole(UserRole userRole) {
+        if (roleExists(userRole)) {
+            return;
+        }
+        if (roles == null) {
+            roles = new ArrayList<>();
+        }
+        roles.add(userRole);
+    }
+
+    private boolean roleExists(UserRole userRole) {
+        return roles != null && roles.stream().anyMatch(r -> userRole.getRoleName().equals(r.getRoleName()));
+    }
+}
 
